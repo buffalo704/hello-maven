e31fe235eea2c06ae5e169fca2abb538300c50de
sso <buffalo704@hotmail.com>
2021-06-10 22:21:20 -0400

    logger and maven functionalities

---
+++ src/main/java/academy/learnprogramming/HelloMaven.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
+    public static void main(String[] args) {
+
+        log.info("Hello Info");
+        log.debug("Hello debug");
+//        System.out.println("Hello Maven");
+    }
+}
